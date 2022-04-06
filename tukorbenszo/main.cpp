dfacf3fc0a7a118f86584b70882b04138020f5ea
TimeaAndrea2002 <47587969+TimeaAndrea2002@users.noreply.github.com>
2022-04-06 19:20:04 +0300

    Add files via upload

---
+++ tukorbenszo/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    int sz=0;
+    string szoveg, megoldas;
+    getline(cin, szoveg);
+    bool kiir=true;
+    for(int i=0; i<szoveg.size(); i++){
+        while(){
+            b=x%10;
+            x=x/10;
+            c=c*10+b;
+        if(szoveg[i]==' '){
+
+        }
+        if(szoveg[i]==' '){
+            kiir=true;
+        }
+        if(kiir){
+            megoldas=megoldas+szoveg[i];
+        }
+    }
+    szoveg=megoldas;
+    cout << szoveg;
+    //cout << "are o selectie reper de desene etamina prezenta";
+    return 0;
+}
