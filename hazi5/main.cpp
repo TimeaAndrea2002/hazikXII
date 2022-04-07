8e90297499bf92e9376099a469766cf24fa0a8c5
TimeaAndrea2002 <47587969+TimeaAndrea2002@users.noreply.github.com>
2022-04-07 10:30:38 +0300

    Add files via upload

--- hazi5/main.cpp
+++ hazi5/main.cpp
@@ -1,39 +1,41 @@
 #include <iostream>
+#include <fstream>
 
 using namespace std;
 
 int main()
 {
-    int n, v[100][100], n1=0, n2=0;
+    ifstream in("input.txt");
+    ofstream out("output.txt");
+    int n,  n1=0, n2=0;
     double s1=0, s2=0;
-    cout << "n=";
-    cin >> n;
+    in >> n;
+    int v[n][n];
     for(int i=0; i<n; i++){
         for(int j=0; j<n; j++){
-            cout << "v[" << i << "][" << j << "]=";
-            cin >> v[i][j];
+            in >> v[i][j];
         }
     }
     for(int i=0; i<n; i++){
         for(int j=0; j<n; j++){
-            if(i<j){
+            if(i<j && v[i][j]>0){
                 s1=s1+v[i][j];
                 n1++;
             }
         }
     }
     s1=s1/n1;
-    cout << s1 << endl;
+    out << s1 << endl;
     for(int i=0; i<n; i++){
         for(int j=0; j<n; j++){
-            if(i>j){
+            if(i>j && v[i][j]>0){
                 s2=s2+v[i][j];
                 n2++;
             }
         }
     }
     s2=s2/n2;
-    cout << s2 << endl;
-    cout << s1-s2;
+    out << s2 << endl;
+    out << s1-s2;
     return 0;
 }
